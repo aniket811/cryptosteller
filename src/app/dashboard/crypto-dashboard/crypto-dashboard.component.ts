a2e60e5b94ada99ec087a46a39fa9dddc2b36700
aniket811 <ajosh4347@gmail.com>
2023-03-25 09:38:26 +0530

    crypto currencies price changes applied

--- src/app/dashboard/crypto-dashboard/crypto-dashboard.component.ts
+++ src/app/dashboard/crypto-dashboard/crypto-dashboard.component.ts
@@ -10,14 +10,16 @@ export class CryptoDashboardComponent  implements OnInit{
   constructor(private cryptoapi:FeaturesService) { }
   public cryptoNews: any[]=[];
   public cryptoPrices: any[]=[];
+ public coinPrices:any[]= []
   ngOnInit() {
     this.cryptoapi.getCryptoNews().subscribe((data:any)=>{
       console.log(data);
       
       this.cryptoNews=data.Data;
     });
-    this.cryptoapi.getCryptoPrices().subscribe((data:any)=>{
-      console.log(this.cryptoPrices);
+    this.cryptoapi.getCryptoPrices().subscribe((datas:any[])=>{
+      this.coinPrices=datas;
+      console.log(this.coinPrices);
     });
   }
 }
