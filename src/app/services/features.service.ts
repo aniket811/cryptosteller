3bb0ac102245181bf09d73271a7714269a331697
aniket811 <ajosh4347@gmail.com>
2023-03-31 21:58:39 +0530

    environements added !

--- src/app/services/features.service.ts
+++ src/app/services/features.service.ts
@@ -1,22 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class FeaturesService {
-
+  weatherapis=environment.weatherapi;
+  cryptoapis=environment.cryptoapi;
+  weatherapikey=environment.weatherapikey;
+  cryptoapikey=environment.cryptoapikey;
   constructor(private http:HttpClient) { }
   public getWeatherData(cityName:string):Observable<any[]>{
-    return this.http.get<any[]>(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4776f3cb0f67c30d16dd152487bef8b6&units=metric
+    return this.http.get<any[]>(`${this.weatherapis}/data/2.5/weather?q=${cityName}&appid=${this.weatherapikey}&units=metric
     `);
   }
   public getCryptoNews():Observable<any[]>{
-    return this.http.get<any[]>(`https://min-api.cryptocompare.com/data/v2/news/?lang=EN&api_key=ab15e7aad9e89183be5097c54e2165d764f0990d14e5763bb07294ae6a6db769`);
+    return this.http.get<any[]>(`${this.cryptoapis}/data/v2/news/?lang=EN&api_key=${this.cryptoapikey}`);
   }
   public getCryptoPrices():Observable<any[]>{
-    return this.http.get<any[]>("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,D,BNB,USDC,XRP,ARB,SHIB,TRX,BUSD&tsyms=USD,EUR,INR")
+    return this.http.get<any[]>(`${this.cryptoapis}/data/pricemulti?fsyms=BTC,ETH,D,BNB,USDC,XRP,ARB,SHIB,TRX,BUSD&tsyms=USD,EUR,INR`)
   }
   public postWeatherData(data:any):Observable<any>{
     return this.http.post<any>("http://ajosh4347-001-site1.dtempurl.com/WeatherForecast/Post",data);
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    weatherapi: 'https://api.openweathermap.org',
+    weatherapikey: '4776f3cb0f67c30d16dd152487bef8b6',
+    cryptoapi: 'https://min-api.cryptocompare.com',
+    cryptoapikey:'ab15e7aad9e89183be5097c54e2165d764f0990d14e5763bb07294ae6a6db769'
+};
