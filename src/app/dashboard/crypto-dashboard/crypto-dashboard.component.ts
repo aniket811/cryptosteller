330c12a9597352eca3b1c8aeccedf826345a4527
Aniket Joshi <aniketjoshi056@gmail.com>
2025-09-10 13:35:46 +0530

    Added Spinner Functionality to improve user experience and avoid flickering inbetween api calls

--- src/app/dashboard/crypto-dashboard/crypto-dashboard.component.ts
+++ src/app/dashboard/crypto-dashboard/crypto-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService, Spinner } from 'ngx-spinner';
 import { FeaturesService } from 'src/app/services/features.service';
 
 @Component({
@@ -7,22 +8,35 @@ import { FeaturesService } from 'src/app/services/features.service';
   styleUrls: ['./crypto-dashboard.component.css']
 })
 export class CryptoDashboardComponent  implements OnInit{
-  constructor(private cryptoapi:FeaturesService) { }
+  constructor(private cryptoapi:FeaturesService,private spinner:NgxSpinnerService) { }
   public cryptoNews: any[]=[];
   public cryptoPrices: any[]=[];
  public coinPrices:any[]= []
   ngOnInit() {
-    this.cryptoapi.getCryptoNews().subscribe((data:any)=>{
-      console.log(data);  
-      this.cryptoNews=data.Data;
-    });
-    this.cryptoapi.getCryptoPrices().subscribe((datas:any[])=>{
-      this.coinPrices=datas;
-      console.log(this.coinPrices);
-    });
+    this.spinner.show();
+      this.getCryptoNews();
+      this.getCryptoPrices();
+     setTimeout(() => {
+    
+      this.spinner.hide();
+    }, 2000);
+
     var continueUser=localStorage.setItem('lastroute','crypto')
   }
   onIconLoadError(err:any){
       err.target.src = "assets/cryptocurrency-icons/svg/color/default.svg"
+  }
+  getCryptoNews(){
+        this.cryptoapi.getCryptoNews().subscribe((data:any)=>{
+      this.cryptoNews=data.Data;
+    });
+  }
+  getCryptoPrices(){
+    
+    this.cryptoapi.getCryptoPrices().subscribe((datas:any[])=>{
+      this.coinPrices=datas;
+       
+      console.log(this.coinPrices);
+    });
   }
 }
