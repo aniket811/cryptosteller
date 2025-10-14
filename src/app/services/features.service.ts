9e7001f410811dcd69f129dde380580ad3748371
Aniket Joshi <aniketjoshi056@gmail.com>
2025-10-14 17:53:07 +0530

    Refactor breach check result display and update data handling logic

--- src/app/services/features.service.ts
+++ src/app/services/features.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
@@ -19,16 +19,24 @@ getCryptoNews(): Observable<any[]> {
   return this.http.get<any[]>(`${this.cryptoapis}/data/v2/news/?lang=EN&api_key=${this.cryptoapikey}`);
 }
    getCryptoPrices(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.cryptoapis}/data/pricemulti?fsyms=BTC,ETH,D,BNB,USDC,XRP,ARB,SHIB,TRX,BUSD&tsyms=USD,EUR,INR`)
+    return this.http.get<any[]>(`${this.cryptoapis}/data/pricemulti?&tsyms=USD,EUR,INR`)
   }
    postWeatherData(data: any): Observable<any> {
     return this.http.post<any>("http://ajosh4347-001-site1.dtempurl.com/WeatherForecast/Post", data);
   }
 getBreachData(email: string): Observable<any> {
-  const noCache = new Date().getTime(); // unique per request
-  return this.http.get<any>(
-    `/leakcheck/api/public?check=${email}&_=${noCache}`
-  );
+  const url = 'https://leakinsight-api.p.rapidapi.com/general/';
+
+  const headers = new HttpHeaders({
+    'x-rapidapi-key': 'a86618140dmshb2c77ff4ecdd30bp1da9b4jsnf43c87115dd4',
+    'x-rapidapi-host': 'leakinsight-api.p.rapidapi.com'
+  });
+
+  const params = new HttpParams()
+    .set('query', email.toLowerCase())
+    .set('type', 'email');
+
+  return this.http.get<any>(url, { headers, params });
 }
 
 }
